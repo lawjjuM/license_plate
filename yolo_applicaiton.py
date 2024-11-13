28c002423f348f024edd558aa11328878a83cbae
Ujjwal sethi <97967727+lawjjuM@users.noreply.github.com>
2024-11-13 21:04:19 +0530

    Update yolo_applicaiton.py

--- yolo_applicaiton.py
+++ yolo_applicaiton.py
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader("Upload an image or video", type=["jpg", "jpeg"
 
 # Load YOLO model
 try:
-    model = YOLO('/Users/A116351061/Documents/License_plate_detection/best.pt')  # Replace with the path to your trained YOLO model
+    model = YOLO('best.pt')  # Replace with the path to your trained YOLO model
 except Exception as e:
     st.error(f"Error loading YOLO model: {e}")
 
