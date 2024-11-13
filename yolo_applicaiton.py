3ad85929afc0f03040fd54f322954f43d2e5c81d
Ujjwal sethi <97967727+lawjjuM@users.noreply.github.com>
2024-11-13 12:03:24 +0530

    Update yolo_applicaiton.py

--- yolo_applicaiton.py
+++ yolo_applicaiton.py
@@ -1,4 +1,4 @@
-import streamlit as st
+aimport streamlit as st
 
 import cv2
 import numpy as np
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader("Upload an image or video", type=["jpg", "jpeg"
 
 # Load YOLO model
 try:
-    model = YOLO('data.pt')  # Replace with the path to your trained YOLO model
+    model = YOLO('best.pt')  # Replace with the path to your trained YOLO model
 except Exception as e:
     st.error(f"Error loading YOLO model: {e}")
 
